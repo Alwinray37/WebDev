a168fb3c1cbbf963a206bf495e949135d7d8ff38
alwinray37 <alwinray37@gmail.com>
2024-05-09 20:22:53 -0700

    updated JS projects home page nav

--- jslearn.js
+++ jslearn.js
@@ -4,25 +4,25 @@
 
 const projContainer = document.querySelector("#project-container");
 const projectCard = document.querySelectorAll(".projectCard");
+const navbarContainer = document.querySelector("#navbarContent");
+const navLibrary = [
+    {"AlwinRay.dev": "https://alwinray.dev"}
+];
+
+// have a consistent navbar throughout each project
+// first clear current html in navbar
+navbarContainer.innerHTML = '';
+navLibrary.forEach(item => {
+    for(let [key, value] of Object.entries(item)){
+        let navHTML = document.createElement('div');
+        navHTML.innerHTML = `
+            <a href="${value}" class="nav-item nav-link">${key}</a>
+        `;
+        navHTML.classList.add("navbar-nav");
+        navbarContainer.append(navHTML);
+    }
+});
 
 
-// project container class list
-projContainer.setAttribute("class", `
-    container 
-    d-flex 
-    flex-column
-    gap-4 
-    flex-wrap 
-    justify-content-center
-`);
-
-// projects class list
-projectCard.forEach((card) => {
-    card.setAttribute("class", `
-        projectCard
-        card 
-        p-2
-    `);
-}); 
 
 
